44219dcc385ba3a691604b7507c9d7f572a013e7
Michele Matteucci <michele.matteucci@phd.units.it>
2025-06-13 11:13:25 +0200

    Commit to merge

--- gamdpy/runtime_actions/__init__.py
+++ gamdpy/runtime_actions/__init__.py
@@ -2,7 +2,6 @@
 from .runtime_action import RuntimeAction, add_runtime_actions_list
 from .scalar_saver import ScalarSaver
 from .trajectory_saver import TrajectorySaver, TimeScheduler
-from .trajectory_saver import TrajectorySaver
 from .restart_saver import RestartSaver
 from .momentum_reset import MomentumReset
 from .stress_saver import StressSaver, extract_stress_tensor
